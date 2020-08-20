d15d10f0cdb01c7ea13c42c1dddef7951db8c670
tshimizu97 <tshimizu.midd@gmail.com>
2020-08-20 11:27:19 +0300

    add some test code for street_view.py

--- gmap_retrieval/street_view.py
+++ gmap_retrieval/street_view.py
@@ -14,7 +14,7 @@ import urllib.parse as urlparse
 
 def sign_url(input_urls=None, secret=None):
     """ Sign a request URL with a URL signing secret.
-    Based on code from https://developers.google.com/maps/documentation/streetview/get-api-key?hl=fi
+    Based on code from https://developers.google.com/maps/documentation/streetview/get-api-key
 
     Parameters
     ----------
@@ -27,7 +27,7 @@ def sign_url(input_urls=None, secret=None):
     -------
     signed_urls: list of str
         The signed request URL
-      """
+    """
 
     if (input_urls is None) or (secret is None):
         raise Exception("Both input_urls and secret are required")
@@ -61,61 +61,92 @@ def sign_url(input_urls=None, secret=None):
     return signed_urls
 
 def get_lat_lon(loc, d, tc):
-    """Calculate the latitude and longitude of a place that is 'd' km away from 'loc' in direction 'tc'.
+    """Calculate the latitude/longitude of place(s) that is 'd' km away from 'loc' in direction 'tc'.
 
     Parameters
     ----------
-    loc: str | list of str
-        a location (or locations) specified by latitude and longitude
-        each location needs to be a comma-separated {latitude,longitude} pair; e.g. "40.714728,-73.998672"
+    loc: str | list of float
+        A location (or locations) specified by latitude and longitude
+        needs to be a comma-separated {latitude,longitude} pair;
+        e.g. "40.714728,-73.998672"
+        If list, lengths must be same as that of 'd' and 'tc'.
     d: float | list of float
-        a distance (or distances) in km from 'loc' to a place to be returned by this function
+        A distance (or distances) in km from 'loc' to a place to be returned
+        by this function.
+        If list, length has to be same as that of 'tc' in case of list.
+        
     tc: float | list of float
-        a direction (or directions) in radians
+        A direction (or directions) in radians.
+        If list, length has to be same as that of 'd' in case of list.
 
     Returns
     -------
     lat_lon: pandas Series of str
-        location(s), specified by latitude and longitude, that is 'd' km away from 'loc' in direction 'tc'
-        each location needs to be a comma-separated {latitude,longitude} pair; e.g. "40.714728,-73.998672"
+        location(s), specified by latitude and longitude,
+        that is 'd' km away from 'loc' in direction 'tc'
+        each location needs to be a comma-separated {latitude,longitude} pair;
+        e.g. "40.714728,-73.998672"
     """
-    if type(d) != type(tc):
-        raise ValueError("d and tc has to be same type: float or list of float.")
-
-    elif type(loc)==str:
-        if type(d)==float:
-            loc = [loc]
+    # if d is array-like
+    if hasattr(d, '__len__'):
+        if not hasattr(tc, '__len__'):
+            raise ValueError("d and tc needs be both float or both array-like.")
+        elif len(d) != len(tc):
+            raise ValueError("The lengths of d and tc needs to be same.")
+          
+    # if loc is array-like
+    if hasattr(loc, '__len__') and not isinstance(loc, str):
+        if not hasattr(d, '__len__'):
+            raise ValueError("If loc is array-like, both d and tc need to be"
+                             "array-like with same length.")
+        if len(loc) != len(d):
+            raise ValueError("If loc is array-like, both d and tc need to be"
+                             "array-like with same length.")
+    
+    # if loc is not array-like
+    else:
+        if hasattr(d, '__len__'):
+            loc = [loc] * len(d)
+            
+        else: # if d and tc is not array-like
             d = [d]
-            tc =[tc]
+            tc = [tc]
+    
+    
+    circumference = 40075 #km
+    if any(np.array(d) > circumference / 2):
+        raise ValueError(f"Distance must be smaller than {circumference / 2}.")
 
-    elif type(d) == float:
-        raise TypeError("If type of loc is list, type of d and tc have to be both list of float.")
-
-    elif len(loc)!=len(d) or len(loc)!=len(tc) or len(d)!=len(tc):
-        raise ValueError("The lengths of loc, d and tc have to be same.")
-
-    loc = np.array([loc.split(",")], dtype=float)
+    if any(np.logical_or(np.array(tc) < 0, np.array(tc) > 2 * np.pi)):
+        raise ValueError("Direction must be between 0 to 2 * Pi.")
+    
+    loc = np.array([l.split(",") for l in loc], dtype=float)
     d = np.array(d)
     tc = np.array(tc)
 
     # convert from km to radians
-    d = d/6371
+    r = 6371 # radius of the earth in km
+    d = d / r
 
-    loc = loc*np.pi/180
+    loc = loc * np.pi / 180
     lat1 = loc[:, 0]
     lon1 = loc[:, 1]
 
-    lat = np.arcsin(np.sin(lat1) * np.cos(d) + np.cos(lat1) * np.sin(d) * np.cos(tc))
-    dlon = np.arctan2(np.sin(tc) * np.sin(d) * np.cos(lat1), np.cos(d) - np.sin(lat1) * np.sin(lat))
+    lat = np.arcsin(np.sin(lat1) * np.cos(d)
+          + np.cos(lat1) * np.sin(d) * np.cos(tc))
+    dlon = np.arctan2(np.sin(tc) * np.sin(d) * np.cos(lat1),
+                      np.cos(d) - np.sin(lat1) * np.sin(lat))
     lon = (lon1 - dlon + np.pi) % (2 * np.pi) - np.pi
 
-    lat_lon = pd.Series((lat * 180 / np.pi), dtype=str) + "," + pd.Series((lon * 180 / np.pi), dtype=str)
+    lat_lon = (pd.Series((lat * 180 / np.pi), dtype=str) + "," +
+               pd.Series((lon * 180 / np.pi), dtype=str))
 
     return lat_lon
 
 def is_gsv_available(API_key, loc, search_radius, outdoor, limit=None):
     """Check if Google street view image is available around specific location(s).
-    Check https://developers.google.com/maps/documentation/streetview/metadata for details of API.
+    Check https://developers.google.com/maps/documentation/streetview/metadata
+    for details of API.
 
     Parameters
     ----------
@@ -170,10 +201,12 @@ def is_gsv_available(API_key, loc, search_radius, outdoor, limit=None):
             return availability
     return availability
 
-def get_street_view_image(directory_name, API_key, secret, IDs, latitude_longitude, n_images,
-                          rad=1, camera_direction=-1, field_of_view=120, angle=0, search_radius=50,
-                          outdoor=True, image_size="640x640", limit=10, n_jobs=1, verbose=True,
-                          if_jupyter=False):
+def get_street_view_image(directory_name, API_key, secret, IDs,
+                          latitude_longitude, n_images, rad=1, 
+                          camera_direction=-1, field_of_view=120, angle=0, 
+                          search_radius=50, outdoor=True,
+                          image_size="640x640", limit=10, n_jobs=1, 
+                          verbose=True, if_jupyter=False):
     """Save Google Street View images around specified locations using Street View Satatic API.
 
     Parameters
@@ -191,15 +224,18 @@ def get_street_view_image(directory_name, API_key, secret, IDs, latitude_longitu
         list of IDs that identify locations
     latitude_longitude: pandas Series [n_locations]
         list of locations specified by latitude and longitude;
-        each location needs to take the form of comma-separated {latitude,longitude} pair; e.g. "40.714728,-73.998672"
+        each location needs to take the form of
+        comma-separated {latitude,longitude} pair; e.g. "40.714728,-73.998672"
     n_images: int
         the number of Google Street View images to be fetched for each ID
     rad: int, optional (default=1)
-        radius, specified in km, of a circle around the location, specified by latitude and longitude, in which
+        radius, specified in km, of a circle around the location,
+        specified by latitude and longitude, in which
         the Google Street View images are fetched
     camera_direction: int, optional (default=-1)
         the compass heading of the camera
-        Accepted values are from 0 to 360 (both values indicating North, with 90 indicating East, and 180 South),
+        Accepted values are from 0 to 360
+        (both values indicating North, with 90 indicating East, and 180 South),
         -1, indicating random selection of headinv value from 0 to 360, and
         -2, indicating the heading value calculated to direct the camera
         towards the location specified by latitude and longitude
@@ -210,23 +246,28 @@ def get_street_view_image(directory_name, API_key, secret, IDs, latitude_longitu
         Positive values angle the camera up (with 90 degrees indicating straight up)
         and negative values angle the camera down (with -90 indicating straight down)
     search_radius: int, optional (default=50)
-        a radius, specified in meters, in which to search for a panorama, centered on the given latitude and longitude
+        a radius, specified in meters, in which to search for a panorama,
+        centered on the given latitude and longitude
     outdoor: boolean, optional (default=True)
         whether or not to limit the search to outdoor photos
     image_size: str, optional (default="400x400")
-        the rectangular dimensions of the map image;  takes the form {horizontal_value}x{vertical_value}
+        the rectangular dimensions of the map image; 
+        takes the form {horizontal_value}x{vertical_value}
     limit: int
         limit the number of trials to find GSV images
-        n_images * limit would be the number of candidate locations to check if GSV available around the area
+        n_images * limit would be the number of candidate locations
+        to check if GSV available around the area
     n_jobs: int, optional (default=1)
-        The number of processes (=number of CPU cores) to use. Specify -1 to use all available cores.
+        The number of processes (=number of CPU cores) to use.
+        Specify -1 to use all available cores.
     verbose: boolean, optional (default=True)
         whether or not to print the progress bar of the data retrieval
     if_jupyter: boolean, optional (default=False)
         whether or not the program is running on Jupyter; this matters only if verbose==True
     """
     if len(IDs) != len(latitude_longitude):
-        raise ValueError("The lengths of IDs and latitude_longitude have to be same.")
+        raise ValueError("The lengths of IDs and latitude_longitude have"
+                         "to be same.")
 
     # create directory in which all the images are saved
     if not os.path.exists(directory_name):
@@ -234,7 +275,8 @@ def get_street_view_image(directory_name, API_key, secret, IDs, latitude_longitu
 
     def wrapped_in_tqdm(IDs):
         if verbose:
-            bar = tqdm(total=len(IDs), mininterval=0, maxinterval=2, miniters=1)
+            bar = tqdm(total=len(IDs), mininterval=0, maxinterval=2,
+                       miniters=1)
         for i in range(len(IDs)):
             if verbose:
                 bar.update(1)
@@ -246,7 +288,8 @@ def get_street_view_image(directory_name, API_key, secret, IDs, latitude_longitu
         id_ = str(IDs[i])
         lat_lon = latitude_longitude[i]
 
-        # create a sub-directory in which 'n_images' Google Street View images around the specified location are saved
+        # create a sub-directory in which 'n_images' Google Street View images
+        # around the specified location are saved
         sub_dir = f"{directory_name}/{id_}"
 
         # if the sub-directory doesn't exist, create a new one
@@ -255,7 +298,8 @@ def get_street_view_image(directory_name, API_key, secret, IDs, latitude_longitu
             n_existing_images = 0
 
         else: # if there are already n_images png images in the sub-directory
-            n_existing_images = len(fnmatch.filter(os.listdir(sub_dir), '*.png'))
+            n_existing_images = len(fnmatch.filter(os.listdir(sub_dir),
+                                                   '*.png'))
             if n_existing_images == n_images: # if there are 'n_images' images in the sub directory
                 return
             else: # if there are some images saved previously, but less than 'n_images'
@@ -267,11 +311,14 @@ def get_street_view_image(directory_name, API_key, secret, IDs, latitude_longitu
         trial_count = 0
         candidate_multiple = 1.5
         while True:
-            # randomly pick 'n_needed_images' * 'candidate_multiple' candidates for locations around 'lat_lon'
-            direction = npr.uniform(0, 2 * np.pi, int(n_images * candidate_multiple))
+            # randomly pick 'n_needed_images' * 'candidate_multiple'
+            # candidates for locations around 'lat_lon'
+            direction = npr.uniform(0, 2 * np.pi,
+                                    int(n_images * candidate_multiple))
             distance = npr.uniform(0, rad, int(n_images * candidate_multiple))
             loc = get_lat_lon(lat_lon, distance, direction)
-            # check if GSV is available for randonly picked 'n_needed_images' * 'candidate_multiple' locations
+            # check if GSV is available for randonly picked
+            # 'n_needed_images' * 'candidate_multiple' locations
             available = is_gsv_available(API_key, loc, search_radius, outdoor, n_images)
             loc_valid_new = loc[available].reset_index(drop=True)
             try: # case of the non-first loop
@@ -298,7 +345,10 @@ def get_street_view_image(directory_name, API_key, secret, IDs, latitude_longitu
         if camera_direction == -2:
             heading = ""
         elif camera_direction == -1:
-            heading = "&heading=" + pd.Series(npr.uniform(0, 360, len(location)).astype(str))
+            heading = ("&heading="
+                       + pd.Series(npr.uniform(0,
+                                               360,
+                                               len(location)).astype(str)))
         else: #when camera_direction is given
             heading = "&heading=" + pd.Series(camera_direction)
         fov = "&fov=" + str(field_of_view)
@@ -310,7 +360,8 @@ def get_street_view_image(directory_name, API_key, secret, IDs, latitude_longitu
             source = ""
         key = "&key=" + API_key
 
-        urls = prefix + location + size + heading + fov + pitch + radius + source + key
+        urls = (prefix + location + size + heading + fov
+                + pitch + radius + source + key)
 
         # add digital signature if provided
         if secret is not None:
@@ -340,8 +391,10 @@ def get_street_view_image(directory_name, API_key, secret, IDs, latitude_longitu
                         f.write(image)
                     break
 
-        # save a CSV file that contains location information about the saved street view images
-        loc_data = pd.DataFrame({'name': new_file_names, 'location': loc_valid})
+        # save a CSV file that contains location information
+        # about the saved street view images
+        loc_data = pd.DataFrame({'name': new_file_names,
+                                 'location': loc_valid})
 
         csv_path = f"{sub_dir}/loc.csv"
         if os.path.exists(csv_path):
@@ -353,4 +406,5 @@ def get_street_view_image(directory_name, API_key, secret, IDs, latitude_longitu
 
     IDs_gen = wrapped_in_tqdm(IDs)
 
-    Parallel(n_jobs)( [delayed(collect_gsv_images_for_each_id)(i) for i in IDs_gen] )
+    Parallel(n_jobs)( [delayed(collect_gsv_images_for_each_id)(i)
+                       for i in IDs_gen] )
